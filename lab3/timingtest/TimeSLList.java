ed5619a9b01763b83d056077c45dc20509b4257a
hyperbolictriangle <hyperbolictriangle@proton.me>
2026-06-08 19:28:06 +0530

    Lab3 (#4)
    * lab3
    
    * Reduce table size due to failing tests on autograder

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -22,7 +22,28 @@ public class TimeSLList {
     }
 
     public static void timeGetLast() {
-        // TODO: YOUR CODE HERE
+        AList<Integer> N = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+        AList<Double> times = new AList<>();
+        SLList<Integer> lst = new SLList<>();
+        int mul = 1;
+        int M = 10000; // number of times getLast is called.
+
+        for (int i = 1; i <= 128000; i++) {
+            lst.addLast(i);
+            if (i == mul * 1000) {
+                // now start timer and perform M getLast operations.
+                Stopwatch time = new Stopwatch();
+                for (int j = 1; j <= M; j++) {
+                    lst.getLast();
+                }
+                N.addLast(i);
+                opCounts.addLast(M);
+                times.addLast(time.elapsedTime());
+                mul <<= 1;
+            }
+        }
+        printTimingTable(N, times, opCounts);
     }
 
 }
