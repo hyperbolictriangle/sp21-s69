25518e3da5a1059cd0605b06e0122cc2b6d30a1b
hyperbolictriangle <hyperbolictriangle@proton.me>
2026-06-08 19:38:59 +0530

    collatz from skeleton

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,24 +3,25 @@
  */
 public class Collatz {
 
-    /** Takes int `n` and returns next number in Collatz Sequence */
+    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 1) {
+        if (n  == 128) {
             return 1;
-        } else if (n%2 == 0) {
-            return n / 2;
-        } else {
+        } else if (n == 5) {
             return 3 * n + 1;
+        } else {
+            return n * 2;
         }
     }
 
     public static void main(String[] args) {
         int n = 5;
+        System.out.print(n + " ");
         while (n != 1) {
-            System.out.print(n + " ");
             n = nextNumber(n);
+            System.out.print(n + " ");
         }
-        System.out.print(n);
+        System.out.println();
     }
 }
 
